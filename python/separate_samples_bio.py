2205bb8b9abdb791d33dcde31e670d89a88a1fc4
Erik <eholum@gmail.com>
2014-12-30 10:13:00 -0500

    initial commit.

---
+++ python/separate_samples_bio.py
@@ -0,0 +1,469 @@
+lbc = {"AACCA":"V6L-0",
+"CCAAC":"V6L-1",
+"GGTTG":"V6L-2",
+"TTGGT":"V6L-3",
+"AGGTG":"V6L-4",
+"CTTAC":"V6L-5",
+"GAACT":"V6L-6",
+"TCCGA":"V6L-7",
+"ACAGT":"V6L-8",
+"CACTG":"V6L-9",
+"GTGAC":"V6L-10",
+"TGTCA":"V6L-11",
+"AAACC":"V6L-12",
+"CCCAA":"V6L-13",
+"GGGTT":"V6L-14",
+"TTTAA":"V6L-15",
+"GAGGT":"V6L-16"}
+
+rbc = {"TGGTT":"V6R-0",
+"GTTGG":"V6R-1",
+"CAACC":"V6R-2",
+"ACCAA":"V6R-3",
+"CACCT":"V6R-4",
+"GTAAG":"V6R-5",
+"AGTTC":"V6R-6",
+"TCGGA":"V6R-7",
+"ACTGT":"V6R-8",
+"CAGTG":"V6R-9",
+"GTCAC":"V6R-10",
+"TGACA":"V6R-11",
+"GGTTT":"V6R-12",
+"TTGGG":"V6R-13",
+"AACCC":"V6R-14",
+"TTAAA":"V6R-15",
+"ACCTC":"V6R-16"}
+
+samples = {"V6L-0V6R-0":"TE.D0B1",
+"V6L-0V6R-1":"TE.D0B2",
+"V6L-0V6R-2":"TE.D0B3",
+"V6L-0V6R-3":"TE.D0B4",
+"V6L-0V6R-4":"TE.D0B5",
+"V6L-0V6R-5":"TE.D0B6",
+"V6L-0V6R-6":"TE.D0B7",
+"V6L-0V6R-7":"TE.D0B8",
+"V6L-0V6R-8":"TE.D0B8",
+"V6L-0V6R-9":"TE.D0.11B",
+"V6L-0V6R-10":"TE.D0.12B",
+"V6L-0V6R-11":"TE.D0.32B",
+"V6L-0V6R-12":"TE.D0.14B",
+"V6L-0V6R-13":"TE.D1.1B",
+"V6L-0V6R-14":"TE.D1.2B",
+"V6L-0V6R-15":"TE.D1.3B",
+"V6L-0V6R-16":"TE.D1.4B",
+"V6L-1V6R-0":"TE.D1.5B",
+"V6L-1V6R-1":"TE.D1.6B",
+"V6L-1V6R-2":"TE.D1.6M",
+"V6L-1V6R-3":"TE.D1.7B",
+"V6L-1V6R-4":"TE.D1.7M",
+"V6L-1V6R-5":"TE.D1.8B",
+"V6L-1V6R-6":"TE.D1.9B",
+"V6L-1V6R-7":"TE.D1.11B",
+"V6L-1V6R-8":"TE.D1.12B",
+"V6L-1V6R-9":"TE.D1.12M",
+"V6L-1V6R-10":"TE.D1.13B",
+"V6L-1V6R-11":"TE.D1.14B",
+"V6L-1V6R-12":"TE.D3a.1B",
+"V6L-1V6R-13":"TE.D3a.1M",
+"V6L-1V6R-14":"TE.D3a.2B",
+"V6L-1V6R-15":"TE.D3a.2M",
+"V6L-1V6R-16":"TE.D3a.3B",
+"V6L-2V6R-0":"TE.D3a.3M",
+"V6L-2V6R-1":"TE.D3a.4B",
+"V6L-2V6R-2":"TE.D3a.4M",
+"V6L-2V6R-3":"TE.D3a.5M",
+"V6L-2V6R-4":"TE.D3a.6B",
+"V6L-2V6R-5":"TE.D3a.7B",
+"V6L-2V6R-6":"TE.D3a.8B",
+"V6L-2V6R-7":"TE.D3a.8M",
+"V6L-2V6R-8":"TE.D3a.9B",
+"V6L-2V6R-9":"TE.D3a.9M",
+"V6L-2V6R-10":"TE.D3a.10B",
+"V6L-2V6R-11":"TE.D3a.11B",
+"V6L-2V6R-12":"TE.D3a.12B",
+"V6L-2V6R-13":"TE.D3a.13B",
+"V6L-2V6R-14":"TE.D3a.13M",
+"V6L-2V6R-15":"TE.D3a.14B",
+"V6L-2V6R-16":"TE.D3b.1B",
+"V6L-3V6R-0":"TE.D3b.2B",
+"V6L-3V6R-1":"TE.D3b.3B",
+"V6L-3V6R-2":"TE.D3b.4B",
+"V6L-3V6R-3":"TE.D3b.5B",
+"V6L-3V6R-4":"TE.D3b.6B",
+"V6L-3V6R-5":"TE.D3b.7B",
+"V6L-3V6R-6":"TE.D3b.8B",
+"V6L-3V6R-7":"TE.D3b.9B",
+"V6L-3V6R-8":"TE.D3b.10B",
+"V6L-3V6R-9":"TE.D3b.11B",
+"V6L-3V6R-10":"TE.D3b.12B",
+"V6L-3V6R-11":"TE.D3b.13B",
+"V6L-3V6R-12":"TE.D3b.14B",
+"V6L-3V6R-13":"TE.D3c.1B",
+"V6L-3V6R-14":"TE.D3c.2B",
+"V6L-3V6R-15":"TE.D3c.3B",
+"V6L-3V6R-16":"TE.D3c.4B",
+"V6L-4V6R-0":"TE.D3c.5B",
+"V6L-4V6R-1":"TE.D3c.6B",
+"V6L-4V6R-2":"TE.D3c.7B",
+"V6L-4V6R-3":"TE.D3c.8B",
+"V6L-4V6R-4":"Beemelmans1",
+"V6L-4V6R-5":"Beemelmans2",
+"V6L-4V6R-6":"Beemelmans3",
+"V6L-4V6R-7":"Beemelmans4",
+"V6L-4V6R-8":"Beemelmans5",
+"V6L-4V6R-9":"CK14Black1",
+"V6L-4V6R-10":"CK14Black2",
+"V6L-4V6R-11":"CK14Black3",
+"V6L-4V6R-12":"CK14Black4",
+"V6L-4V6R-13":"CK14Black5",
+"V6L-4V6R-14":"CK14Black6",
+"V6L-4V6R-15":"CK14Orange1",
+"V6L-4V6R-16":"CK14Orange2",
+"V6L-5V6R-0":"CK14Orange3",
+"V6L-5V6R-1":"CK14Orange4",
+"V6L-5V6R-2":"CK14Orange5",
+"V6L-5V6R-3":"CK14Orange6",
+"V6L-5V6R-4":"CK14Red1",
+"V6L-5V6R-5":"CK14Red2",
+"V6L-5V6R-6":"CK14Red3",
+"V6L-5V6R-7":"CK14Red4",
+"V6L-5V6R-8":"CK14Red5",
+"V6L-5V6R-9":"CK14White1",
+"V6L-5V6R-10":"CK14White2",
+"V6L-5V6R-11":"CK14White3",
+"V6L-5V6R-12":"CK14White4",
+"V6L-5V6R-13":"CK14White5",
+"V6L-5V6R-14":"CK14White6",
+"V6L-5V6R-15":"CK14Yellow1",
+"V6L-5V6R-16":"CK14Yellow2",
+"V6L-6V6R-0":"CK14Yellow3",
+"V6L-6V6R-1":"CK14Yellow4",
+"V6L-6V6R-2":"CK14Yellow5",
+"V6L-6V6R-3":"CK14Yellow6",
+"V6L-6V6R-4":"P2.T1",
+"V6L-6V6R-5":"P2.D3",
+"V6L-6V6R-6":"P2.T3",
+"V6L-6V6R-7":"P2.D4",
+"V6L-6V6R-8":"P2.T4",
+"V6L-6V6R-9":"P2.T5",
+"V6L-6V6R-10":"P2.D6",
+"V6L-6V6R-11":"P2.T7",
+"V6L-6V6R-12":"P2.45",
+"V6L-6V6R-13":"CK4.D3",
+"V6L-6V6R-14":"CK4.D4",
+"V6L-6V6R-15":"CK5.D6",
+"V6L-6V6R-16":"CK5.D7",
+"V6L-7V6R-0":"CK5.D8",
+"V6L-7V6R-1":"CK5.33",
+"V6L-7V6R-2":"CK6.D1",
+"V6L-7V6R-3":"CK6.D2",
+"V6L-7V6R-4":"CK6.D3",
+"V6L-7V6R-5":"CK6.D4",
+"V6L-7V6R-6":"CK6.D7",
+"V6L-7V6R-7":"CK6.T7",
+"V6L-7V6R-8":"CK6.D8",
+"V6L-7V6R-9":"CK6.T8",
+"V6L-7V6R-10":"CK6.T70",
+"V6L-7V6R-11":"P2.HI.4",
+"V6L-7V6R-12":"P2.DI.4",
+"V6L-7V6R-13":"CK4.HI.4",
+"V6L-7V6R-14":"CK4.DI.4",
+"V6L-7V6R-15":"CK5.DI.7",
+"V6L-7V6R-16":"CK5.HI.10",
+"V6L-8V6R-0":"CK5.DI.10",
+"V6L-8V6R-1":"SR4.6O",
+"V6L-8V6R-2":"SR4.6G",
+"V6L-8V6R-3":"SR4.8O",
+"V6L-8V6R-4":"SR4.8G",
+"V6L-8V6R-5":"SR4.14O",
+"V6L-8V6R-6":"SR4.14G",
+"V6L-8V6R-7":"SR4.18O",
+"V6L-8V6R-8":"SR4.18G",
+"V6L-8V6R-9":"SR4.20O",
+"V6L-8V6R-10":"SR4.20G",
+"V6L-8V6R-11":"SR4.27O",
+"V6L-8V6R-12":"SR4.27G",
+"V6L-8V6R-13":"SR4.31O",
+"V6L-8V6R-14":"SR4.31G",
+"V6L-8V6R-15":"SR4.35O",
+"V6L-8V6R-16":"SR4.35G",
+"V6L-9V6R-0":"SR4.CA.6",
+"V6L-9V6R-1":"SR4.CA.21",
+"V6L-9V6R-2":"SR4.CA.29",
+"V6L-9V6R-3":"SR4.CA.11",
+"V6L-9V6R-4":"SR4.CA.7",
+"V6L-9V6R-5":"SR4.CA.1",
+"V6L-9V6R-6":"SR4.CA.10",
+"V6L-9V6R-7":"SR4.CA.19",
+"V6L-9V6R-8":"SR4.CC.1",
+"V6L-9V6R-9":"SR4.CC.2",
+"V6L-9V6R-10":"SR4.CC.3",
+"V6L-9V6R-11":"1.1D",
+"V6L-9V6R-12":"1.2D",
+"V6L-9V6R-13":"1.3D",
+"V6L-9V6R-14":"1.4D",
+"V6L-9V6R-15":"1.3H",
+"V6L-9V6R-16":"1.4H",
+"V6L-10V6R-0":"2.1D",
+"V6L-10V6R-1":"2.2D",
+"V6L-10V6R-2":"2.3D",
+"V6L-10V6R-3":"2.4D",
+"V6L-10V6R-4":"2.3H",
+"V6L-10V6R-5":"2.4H",
+"V6L-10V6R-6":"3.1D",
+"V6L-10V6R-7":"3.2D",
+"V6L-10V6R-8":"3.3D",
+"V6L-10V6R-9":"3.4D",
+"V6L-10V6R-10":"3.3H",
+"V6L-10V6R-11":"3.4H",
+"V6L-10V6R-12":"4.1D",
+"V6L-10V6R-13":"4.2D",
+"V6L-10V6R-14":"4.3D",
+"V6L-10V6R-15":"4.4D",
+"V6L-10V6R-16":"4.3H",
+"V6L-11V6R-0":"4.4H",
+"V6L-11V6R-1":"5.1D",
+"V6L-11V6R-2":"5.2D",
+"V6L-11V6R-3":"5.3D",
+"V6L-11V6R-4":"5.4D",
+"V6L-11V6R-5":"5.3H",
+"V6L-11V6R-6":"5.4H",
+"V6L-11V6R-7":"P2.WBD1.2010",
+"V6L-11V6R-8":"P2.T47.2010",
+"V6L-11V6R-9":"P2.E.WBD.B1",
+"V6L-11V6R-10":"P2.T47.2009",
+"V6L-11V6R-11":"P2.WBD2.2010",
+"V6L-11V6R-12":"P2.T46.2010",
+"V6L-11V6R-13":"P2.T46.2009",
+"V6L-11V6R-14":"P2.E.WBD.B2",
+"V6L-11V6R-15":"P2.WBD3.2010",
+"V6L-11V6R-16":"P2.T44.2010",
+"V6L-12V6R-0":"P2.WBD5.2009",
+"V6L-12V6R-1":"P2.T44.2009",
+"V6L-12V6R-2":"P2.T38.2010",
+"V6L-12V6R-3":"P2.T38.WBD.2010",
+"V6L-12V6R-4":"P2.E.WBD.B3.2009",
+"V6L-12V6R-5":"P2.T2.2009",
+"V6L-12V6R-6":"P2.T42.2010",
+"V6L-12V6R-7":"P2.WBD1.2009",
+"V6L-12V6R-8":"P2.T6.2009",
+"V6L-12V6R-9":"P2.T41.2010",
+"V6L-12V6R-10":"P2.WBD2.2009",
+"V6L-12V6R-11":"P2.T5.2009",
+"V6L-12V6R-12":"P2.T37.2010",
+"V6L-12V6R-13":"P2.T39.2010",
+"V6L-12V6R-14":"CK4.T8.2010",
+"V6L-12V6R-15":"CK4.T8.WBD2010",
+"V6L-12V6R-16":"CK4.T8.2009",
+"V6L-13V6R-0":"CK4.WBD8.2009",
+"V6L-13V6R-1":"CK4.T4.WBD.2010",
+"V6L-13V6R-2":"CK4.T4.2010",
+"V6L-13V6R-3":"CK4.E.WBD.B4.2009",
+"V6L-13V6R-4":"CK4.T4.2009",
+"V6L-13V6R-5":"CK4.T1.WBD.2010",
+"V6L-13V6R-6":"CK4.T1.2010",
+"V6L-13V6R-7":"CK4.T1.2009",
+"V6L-13V6R-8":"CK4.WBD1.2009",
+"V6L-13V6R-9":"CK4.T7.2010",
+"V6L-13V6R-10":"CK4.T7.2009",
+"V6L-13V6R-11":"CK4.WBD7.2009",
+"V6L-13V6R-12":"CK4.T6.2010",
+"V6L-13V6R-13":"CK4.T6.2009",
+"V6L-13V6R-14":"CK4.WBD6.2009",
+"V6L-13V6R-15":"CK4.T5.2010",
+"V6L-13V6R-16":"CK4.T5.2009",
+"V6L-14V6R-0":"CK4.T5WBD.2009",
+"V6L-14V6R-1":"CK4.WBD5.2009",
+"V6L-14V6R-2":"CK4.T2.2010",
+"V6L-14V6R-3":"CK4.T2.2009",
+"V6L-14V6R-4":"CK4.WBD2.2009",
+"V6L-14V6R-5":"CK5.WBD1.2010",
+"V6L-14V6R-6":"CK5.T26.2010",
+"V6L-14V6R-7":"CK5.WBD1.2009",
+"V6L-14V6R-8":"CK5.T1.2009",
+"V6L-14V6R-9":"CK5.WBD2.2010",
+"V6L-14V6R-10":"Beemelmans6",
+"V6L-14V6R-11":"Beemelmans7",
+"V6L-14V6R-12":"Beemelmans8",
+"V6L-14V6R-13":"CK5.WBD3.2010",
+"V6L-14V6R-14":"CK5.T28.2010",
+"V6L-14V6R-15":"CK5.WBD3.2009",
+"V6L-14V6R-16":"CK5.T3.2009",
+"V6L-15V6R-0":"CK5.WBD4.2010",
+"V6L-15V6R-1":"CK5.T32.2010",
+"V6L-15V6R-2":"CK5.WBD4.2009",
+"V6L-15V6R-3":"CK5.T4.2009",
+"V6L-15V6R-4":"CK5.WBD5.2010",
+"V6L-15V6R-5":"CK5.WBD5.2009",
+"V6L-15V6R-6":"CK5.T5.2009",
+"V6L-15V6R-7":"CK5.WBD6.2010",
+"V6L-15V6R-8":"CK5.T6.2009",
+"V6L-15V6R-9":"CK6.E.WBDB3.2009",
+"V6L-15V6R-10":"CK6.T19.2010",
+"V6L-15V6R-11":"CK6.WBD6.2010",
+"V6L-15V6R-12":"CK6.WBD6.2009",
+"V6L-15V6R-13":"CK6.T6.2009",
+"V6L-15V6R-14":"CK6.WBD5.2010",
+"V6L-15V6R-15":"CK6.T17.2010",
+"V6L-15V6R-16":"CK6.T5.2009",
+"V6L-16V6R-0":"CK6.WBD5.2009",
+"V6L-16V6R-1":"CK6.WBD4.2010",
+"V6L-16V6R-2":"CK6.T16.2010",
+"V6L-16V6R-3":"CK6.WBD4.2009",
+"V6L-16V6R-4":"CK6.T4.2009",
+"V6L-16V6R-5":"CK6.WBD3.2010",
+"V6L-16V6R-6":"CK6.T15.2010",
+"V6L-16V6R-7":"CK6.WBD3.2009",
+"V6L-16V6R-8":"CK6.T3.2009",
+"V6L-16V6R-9":"CK6.WBD2.2010",
+"V6L-16V6R-10":"CK6.T14.2010",
+"V6L-16V6R-11":"CK6.T2.2009",
+"V6L-16V6R-12":"Beemelmans9",
+"V6L-16V6R-13":"Beemelmans10",
+"V6L-16V6R-14":"Beemelmans11",
+"V6L-16V6R-15":"Beemelmans12",
+"V6L-16V6R-16":"Beemelmans13"}
+
+files = {}
+
+filter_limit = 20
+
+import gc
+	
+def create_files(o):
+	for key in samples:
+		name = samples[key]
+		t = o + name + "_CBBS.fastq"
+		files[name] = (t,[])
+		
+	files["NO_MATCH"] = (o + "NO_MATCH.fastq",[])
+	files["LOW_QUALITY_READS"] = (o + "LOW_QUALITY_READS.fastq",[])
+	
+	for key in files:
+		f = open(files[key][0], 'w')
+		f.close()
+	
+def dump_files():
+	for key in files:
+		t = files[key]
+		f = open(t[0],'a')
+		
+		for line in t[1]:
+			f.write(line)
+
+		files[key] = (t[0],[])
+		f.close()
+	
+	print gc.collect()
+		
+
+def separate(i):
+	
+	from Bio import SeqIO
+	from time import time
+	
+	data = SeqIO.parse(i,"fastq")
+	
+	count = 0
+	filtered = 0
+	
+	start = time()
+	for read in data:
+		if count % 25000 == 0 and count > 1:
+			print "processed " + str(count) + " reads in " + str(time() - start) + " seconds (" + str(((filtered+0.0)/count)*100) + "% have been filtered)"
+		
+		d = str(read.seq)
+		f = get_sample_name(read)
+		
+		if (f == "LOW_QUALITY_READS"):
+			filtered += 1
+			
+		temp = files[f]
+		temp[1].append(format(read,"fastq"))
+		
+		count += 1
+		
+		if count % 500000 == 0:
+			print "DUMPING FILES... "
+			dump_files()
+			
+	print "analysis complete, successfully processed " + str(count) + " reads... writing to files"
+	dump_files()
+	
+	print "files written, filtered " + str(filtered) + " reads (" + str(((filtered+0.0)/count)*100) + "%)"
+		
+def get_sample_name(rec):
+	
+	d = str(rec.seq)
+	leftBC = d[0:5]
+	rightBC = d[-5:]
+	
+	if not ((leftBC in lbc)	and (rightBC in rbc)):
+		return "NO_MATCH"
+	
+	if compute_phred_score(rec) <= filter_limit:
+		return "LOW_QUALITY_READS"
+		
+	ids = lbc[leftBC] + rbc[rightBC]
+	
+	return samples[ids]
+	
+def compute_phred_score(rec):
+	phreds = rec.letter_annotations["phred_quality"]
+	
+	lps = phreds[0:5]
+	rps = phreds[-5:]
+	
+	low_score = 60
+	
+	for i in xrange(len(lps)):
+		if lps[i] > 2:
+			low_score = min(lps[i],low_score)
+		if rps[i] > 2:
+			low_score = min(rps[i],low_score)
+
+	return low_score
+	
+def expand_maps():
+	
+	for key in lbc.keys():
+		value = lbc[key]
+		
+		for i in xrange(len(key)):
+			lbc[key[0:i] + "N" + key[i+1:]] = value
+			lbc[key[0:i] + "A" + key[i+1:]] = value
+			lbc[key[0:i] + "C" + key[i+1:]] = value
+			lbc[key[0:i] + "T" + key[i+1:]] = value
+			lbc[key[0:i] + "G" + key[i+1:]] = value
+			
+	for key in rbc.keys():
+		value = rbc[key]
+		
+		for i in xrange(len(key)):
+			rbc[key[0:i] + "N" + key[i+1:]] = value
+			rbc[key[0:i] + "A" + key[i+1:]] = value
+			rbc[key[0:i] + "C" + key[i+1:]] = value
+			rbc[key[0:i] + "T" + key[i+1:]] = value
+			rbc[key[0:i] + "G" + key[i+1:]] = value
+		
+
+if __name__ == "__main__":
+	
+	import sys
+	
+	i = sys.argv[1]
+	o = sys.argv[2]
+
+	if (len(sys.argv) > 3):
+		filter_limit = int(sys.argv[3])
+
+	print "reading from: " + i
+	print "writing to: " + o
+	
+	if not o[-1] == "/":
+		o = o + "/"
+		
+	expand_maps()
+	create_files(o)
+	separate(i)
+		
