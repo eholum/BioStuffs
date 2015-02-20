e85f51105b4219c9ce1609c0681725f17f4ba506
Erik <eholum@gmail.com>
2015-02-20 15:26:08 -0700

    Slight optimization.

--- c/ccount.c
+++ c/ccount.c
@@ -7,28 +7,33 @@
 #include <stdarg.h>
 #include <string.h>
 
+#define ASCII_SIZE 256 /* Extended ascii */
+
 typedef unsigned long count; /* Counter */
 
 /* Current file counters */
-static count ccount[256];
+static count ccount[ASCII_SIZE];
 count lcount;
 
 /* Total files counter */
-static count t_ccount[256];
+static count t_ccount[ASCII_SIZE];
 count t_lcount;
 
 void report (char *file, count *ccount, count lcount, char *s) 
 {
     int i;
-    
-    for (i = 0; s[i] != '\0'; i++) {
-        printf("%6c ", *(s + i));
-    }
-    printf("lines file \n");
     for (i = 0; *(s + i) != '\0'; i++) {
         printf("%6lu ", ccount[*(s + i)]);
     }
     printf("%6lu %s\n ", lcount, file);
+}
+
+void reset (count *ccount, char *s) {
+    int i;
+    memset(ccount, -1L, ASCII_SIZE);
+    for (i = 0; *(s + i) != '\0'; i++) {
+       ccount[*(s + i)] = 0L;    
+    }
 }
 
 void counter(char *file, char *s) 
@@ -42,13 +47,20 @@ void counter(char *file, char *s)
         return;
     }
     
-    memset(ccount, 0L, 256);
+    reset(ccount, s);
     lcount = 0L;
     while ((c = getc(fp)) != EOF ) {
+        // Increment line count first
         if (c == '\n') {
-            lcount++; t_lcount++;
+            lcount++; 
+            t_lcount++;
         }
-        (ccount[c])++; (t_ccount[c])++;
+        // If we don't care about the char, skip it
+        if (ccount[c] == -1L)
+            continue;
+        
+        (ccount[c])++;
+        (t_ccount[c])++;
     }
     
     report (file, ccount, lcount, s);
@@ -63,6 +75,12 @@ int main(int argc, char **argv)
         return -1;
     }
     
+    for (i = 0; argv[1][i] != '\0'; i++) {
+        printf("%6c ", argv[1][i]);
+    }
+    printf("lines file \n");
+    
+    reset (t_ccount, argv[1]);
     for (i = 2; i < argc; i++)
         counter (argv[i], argv[1]);
     
